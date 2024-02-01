2e6be34f415438fa35c1b51b7050dbd51e2332f2
ligang <ligang@uhrongyi.cn>
2024-02-01 14:26:11 +0800

    优化代码，删除冗余类

--- rds-proxy/proxy/src/main/java/com/tongtech/proxy/Version.java
+++ rds-proxy/proxy/src/main/java/com/tongtech/proxy/Version.java
@@ -3,5 +3,5 @@ package com.tongtech.proxy;
 public class Version {
     public static final String Version ="2.2.1.4";
     public static final String ProductName = "TongRDS Proxy";
-    public static final String BuildTime = "2023-12-29 16:57:54";
+    public static final String BuildTime = "2024-02-01 14:24:59";
 }
