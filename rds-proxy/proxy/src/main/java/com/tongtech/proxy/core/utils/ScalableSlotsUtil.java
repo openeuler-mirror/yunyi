125e9bf730070c90b16c0807b11e44ae6eda7d45
ligang <ligang@uhrongyi.cn>
2024-05-21 09:01:27 +0800

    更新proxy计算分片位置的算法，可通过key的取值控制同类key进入相同分片。该算法对应TongRDS企业版2.2.1.5和云版2.2.C.2。

--- rds-proxy/proxy/src/main/java/com/tongtech/proxy/core/utils/ScalableSlotsUtil.java
+++ rds-proxy/proxy/src/main/java/com/tongtech/proxy/core/utils/ScalableSlotsUtil.java
@@ -6,61 +6,16 @@ public class ScalableSlotsUtil {
      */
     public static volatile int TOTAL_BLOCKS = 0;
 
-    public static int[] createBlockByRanges(String ranges) {
-        int[] range = new int[TOTAL_BLOCKS];
-        if (ranges != null && ranges.length() > 0) {
-            String[] rs = ranges.split(",");
-            for (String r : rs) {
-                int min = -1;
-                int max = -1;
-                try {
-                    String[] reps = r.split("-");
-                    min = Integer.parseInt(reps[0].trim());
-                    try {
-                        max = Integer.parseInt(reps[1].trim());
-                    } catch (Throwable t) {
-                        max = min;
-                    }
-                } catch (Throwable t) {
-                }
-                if (min >= 0 && max >= 0 && min <= max) {
-                    for (int i = min; i <= max; ++i) {
-                        if (i < TOTAL_BLOCKS) {
-                            range[i] = 1;
-                        }
-                    }
-                }
-            }
-        }
-        return range;
-    }
-
-    public static boolean isInBlock(byte[] key, int[] blocks) {
-        int hash = 0;
-        if (key != null && blocks != null && blocks.length > 0) {
-            for (int i = 0; i < key.length; ++i) {
-                int c = key[i] & 0xff;
-                int bit = (i * 7) % 13;
-                if (bit > 9) {
-                    hash ^= c >> (13 - bit);
-                } else {
-                    hash ^= (c << bit) & 0xffff;
-                }
-            }
-            return blocks[hash % blocks.length] != 0;
-        } else if (blocks == null || blocks.length == 0) {
-            // blocks 为空
-            return true;
-        } else {
-            // key 为空
-            return false;
-        }
-    }
-
-    public static int hashKey(byte[] key) {
+    /**
+     * 旧版本使用的hash算法
+     *
+     * @param key
+     * @return
+     */
+    public static int old_hashKey(byte[] key) {
         // 未成功连接Center时，该值为“0”
         if (TOTAL_BLOCKS <= 0) {
-           throw new IllegalStateException("ERR unauthorized from center");
+            throw new IllegalStateException("ERR unauthorized from center");
         }
 
         int hash = 0;
@@ -75,4 +30,19 @@ public class ScalableSlotsUtil {
         }
         return hash % TOTAL_BLOCKS;
     }
+
+    /**
+     * TongRDS 2.2.1.5和TongRDS-CN 2.2.C.2以后使用的新算法
+     *
+     * @param key
+     * @return
+     */
+    public static int hashKey(byte[] key) {
+        // 未成功连接Center时，该值为“0”
+        if (TOTAL_BLOCKS <= 0) {
+            throw new IllegalStateException("ERR unauthorized from center");
+        }
+        return RedisSlotCRC.getSlot(key) % TOTAL_BLOCKS;
+    }
+
 }
